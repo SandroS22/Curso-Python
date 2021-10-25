a81db4fbf3d262e1e7f974f42fcc16e3961ab55d
SandroS22 <62521881+SandroS22@users.noreply.github.com>
2021-10-24 23:11:14 -0300

    Add files via upload

---
+++ atv29.py
@@ -0,0 +1,6 @@
+vel = int(input('Velocidade do veículo em Km/h: '))
+
+if vel > 80:
+    print('Você foi multado por excesso de velocidade.'
+          '\nSerá cobrado em R$7,00 para cada Km/h acima da permitida. ')
+
